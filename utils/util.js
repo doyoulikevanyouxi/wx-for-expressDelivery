10bdea69bea39acda58eb7b1191a92a79720772a
Shimakaze9490 <949034746@qq.com>
2018-03-31 22:45:29 +0800

    add test

--- utils/util.js
+++ utils/util.js
@@ -1,23 +1,3 @@
-// const formatTime = date => {
-//   const year = date.getFullYear()
-//   const month = date.getMonth() + 1
-//   const day = date.getDate()
-//   const hour = date.getHours()
-//   const minute = date.getMinutes()
-//   const second = date.getSeconds()
-
-//   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-// }
-
-// const formatNumber = n => {
-//   n = n.toString()
-//   return n[1] ? n : '0' + n
-// }
-
-// module.exports = {
-//   formatTime: formatTime
-// }
-
 
 /*   项目基本配置  */
 let util = {
