e1a61e088cebbe9e0b5235788f182655a8cae8b2
949034746@qq.com <air@airs-MacBook-Air.local>
2018-03-31 22:01:10 +0800

    app.js LoginIn

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wx-for-expressDelivery
-wx-for-expressDelivery
+build the wx frame.
+
+1.思考下在app.js还是index.js处理用户登录和userInfo.
+2.index.js 如何获取 app.js 的全局变量.
+3.注意在app.js中不能调用this.setData()函数,用this.global.userInfo = res.userInfo. 或者用缓存
+4.如何处理用户拒绝权限后,再次请求.
--- app.js
+++ app.js
@@ -2,84 +2,105 @@ let util = require("./utils/util.js");
 
 App({
   //全局变量    
-  globalDate: {
+  globalData: {
+    text: "123",
     userInfo: {},
     // api请求接口
     domain: "https://xxx.com/",
   },
   onHide: function() {
-    wx.removeStorageSync("TY_storage");  //隐藏后清除"TY_storage"缓存
+    // wx.removeStorageSync("TY_storage");  //隐藏后清除"TY_storage"缓存
   },
+
+  //暂未使用 options, 需要跳转参数时使用
   onShow: function(options) {
-    // let userInfo = wx.getStorageSync("userInfo");
-    // if (!userInfo) {
-    //   this.userLogin(options);
-    // }
+    let userInfo = wx.getStorageSync("userInfo");
+    if (!userInfo) {
+      this.userLogin(options);
+    };
+    this.globalData.text = "1111";
+    // console.log(this.globalData.text);
   },
   //登陆函数
-  // userLogin: function(options) {
-  //   let that = this;
-  //   wx.login({
-  //     success: function(res) {
-  //       if (res.code) {
-  //         //发起网络请求
-  //         that.getUserInfo(res.code, options);
-  //       } else {
-  //         util.loadingToast(true, {
-  //           title: res.errMsg
-  //         });
-  //       }
-  //     },
-  //   });
-  // },
-  // //调用微信getUserInfo api
-  // getUserInfo: function() {
-  //   let that = this;
-  //   util.loadingToast(true, {
-  //     title: "获取用户资料"
-  //   });
-  //   wx.getUserInfo({
-  //     lang: "zh_CN"
-  //   });
-  // },
-
-  //默认配置
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
+  userLogin: function(options) {
+    let that = this;
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+      success: function(res) {
+        if (res.code) {
+          //发起网络请求
+          that.getUserInfo(res.code, options);
+        } else {
+          util.loadingToast(true, {
+            title: res.errMsg
+          });
         }
-      }
-    })
+      },
+    });
   },
-  globalData: {
-    userInfo: null
+  //调用微信getUserInfo api
+  getUserInfo: function(code,options) {
+    let that = this;
+    util.loadingToast(true, {
+      title: "获取用户资料"
+    });
+    wx.getUserInfo({
+      lang: "zh_CN",
+      encryptedData: true,
+      success: function(res) {
+        that.globalData.userInfo = res.userInfo;  //用户信息存入全局变量
+        console.log(that.globalData.userInfo);
+        //此处保存用户信息,异步接口
+        // wx.setStorage({
+        //   key: "userInfo",
+        //   data: res.userInfo,
+        //   success: function() {
+        //     console.log("get userInfo success!")
+        //   }
+        // });
+        util.loadingToast(false);
+      },
+      fail: function() {  //用户拒绝获取用户信息
+        util.loadingToast(false);
+        util.dialog({
+          content: "获取用户信息失败",
+          showCancel: false
+        });
+        //获取失败或者用户拒绝:后需要处理!
+        // that.userLogin(options);
+      },
+      complete: function() {
+
+      }
+    });
+  },
+
+  // 与系统后台进行交互 传入获取的userInfo和code  方便后台存储用户信息. 暂不确定是否需要.
+  submitUserInfo: function(wxRes,code,options) {
+    let that = this;
+    let userInfo = wxRes.userInfo;
+    util.loadingToast(true,{
+      title: "登录中..."
+    });
+
+    //发送请求
+    let config = {
+      url: "",
+      data: "",
+      method: "POST",
+      success: function() {
+
+      }
+    };
+    // util.sendAjax(config);
   }
+
 });
 
 module.exports = getApp();
+
+
+
+
+
+
+
