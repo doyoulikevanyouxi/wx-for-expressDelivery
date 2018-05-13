d34eea30ae6127ab78fb6731a0d5484068445c0d
Shimakaze9490 <949034746@qq.com>
2018-05-13 12:21:54 +0800

    rechange the index.js

--- app.js
+++ app.js
@@ -1,42 +1,100 @@
-/* 修改后的app.js */
+/* 原先的app.js */
 let util = require("./utils/util.js");
-
+let app = getApp();
+// console.log(app);
 App({
+  // 全局变量
   globalData: {
-    userInfo: null
-    // domain: "http://xxx.com/"
+    userInfo: undefined,
+    // api请求接口
+    domain: "https://xxx.com/"
   },
+
   onLaunch: function() {
-    //调用API从本地缓存获取数据.用数组记录登陆时间
-    let logs = wx.getStorageSync("logs") || [];
-    logs.unshift(Date.now());
-    wx.setStorageSync("logs",logs);
-    console.log("调用getUserInfo");
-    this.getUserInfo();
+    // console.log("app-onLaunch");
   },
-  getUserInfo: function(cb) {
+  onHide: function() {
+    // wx.removeStorageSync("TY_storage");  //隐藏后清除"TY_storage"缓存
+  },
+
+  onLoad: function() {
+    // console.log("app-onLoad");
+  },
+  // 暂未使用 options, 需要跳转参数时使用
+  onShow: function(options) {
+    // console.log("app-onShow");
+    // let userInfo = wx.getStorageSync("userInfo");
+    // if (!userInfo) {
+    //   this.userLogin(options);
+    // }
+  },
+  // 登陆函数
+  userLogin: function(options) {
     let that = this;
-    // 执行回调函数
-    if(this.globalData.userInfo) {
-      typeof cb === "function" && cb(this.globalData.userInfo);
-    } else {
-      // 调用登陆接口,login
-      wx.login({
-        success: function() {
-          // 获取用户信息jiekougetUserInfo
-          wx.getUserInfo({
-            success: function(res) {
-              that.globalData.userInfo = res.userInfo;
-              typeof cb === "function" && cb(this.globalData.userInfo);
-            }
-          })
+    wx.login({
+      success: function(res) {
+        if (res.code) {
+          // 发起网络请求
+          that.getUserInfo(res.code, options);
+        } else {
+          util.loadingToast(true, {
+            title: res.errMsg
+          });
         }
-      })
+      }
+    });
+  },
+  // 调用微信getUserInfo api
+  getUserInfo: function(code, options) {
+    let that = this;
+    util.loadingToast(true, {title: "获取用户资料"});
+
+    // 此处需要用户授权,造成异步
+    wx.getUserInfo({
+      lang: "zh_CN",
+      encryptedData: true,
+      success: function(res) {
+        // console.log(res.userInfo);
+        wx.setStorageSync("userInfo",res.userInfo);
+
+        // console.log(app);
+        // eslint-disable-next-line
+        util.loadingToast(false);
+      },
+      // 用户拒绝获取用户信息
+      fail: function() {
+        util.loadingToast(false);
+        util.dialog({
+          content: "您没有授权,无法获取信息",
+          showCancel: false
+        });
+        // 获取失败或者用户拒绝:后需要处理!
+        // that.userLogin(options);
+      },
+      complete: function() {
+
+      }
+    });
+  },
+
+  // 与系统后台进行交互 传入获取的userInfo和code  方便后台存储用户信息. 暂不确定是否需要.
+  submitUserInfo: function(wxRes, code, options) {
+    let that = this;
+    let userInfo = wxRes.userInfo;
+    util.loadingToast(true, {
+      title: "登录中..."
+    });
+
+    // 发送请求
+    let config = {
+      url: "",
+      data: "",
+      method: "POST",
+      success: function() {
+
+      }
     };
+    // util.sendAjax(config);
   }
 });
-//导出模块
-module.exports = {
-  getApp : getApp(),
-  userInfo : getApp().globalData.userInfo
-}
+module.exports = getApp();
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,104 @@
 let util = require("../../utils/util");
 // 获取应用实例
-const app = getApp();
-
+// const app = getApp();
 Page({
   data: {
     userInfo: undefined,
-    motto: "Welcome to 快递帮"
-    // hasUserInfo: false,
-    // canIUse: wx.canIUse('button.open-type.getUserInfo')
+    motto: "Welcome to 快递帮",
+    //是否加载完成
+    isLoaded: false,
   },
-
   onLoad: function() {
     console.log("onLoad!");
   },
-
   // 注意:此处获取userInfo要在,onShow生命周期,而不能在onLoad中
-  onShow: function() {
-    console.log("onShow");
-    
-    // this.setData({
-    //   // userInfo : app.globalData.userInfo
-    //   userInfo : wx.getStorageSync("userInfo")
-    // });
-    // console.log(this.data.userInfo);
+  onShow: function(options) {
+    console.log("onShow!");
+    let that = this;
+    let userInfo = that.data.userInfo;
+    //如果为空
+    if(!userInfo) {
+      this.userLogin(options);
+    }
   },
-
   onHide: function() {
 
   },
+  userLogin: function(options) {
+    let that = this;
+    wx.login({
+      success: function(res) {
+        if (res.code) {
+          // 发起网络请求
+          that.getUserInfo(res.code, options);
+        } else {
+          util.loadingToast(true, {
+            title: res.errMsg
+          });
+        }
+      }
+    })
+  },
+  // 调用微信getUserInfo api
+  getUserInfo: function(code, options) {
+    let that = this;
+    util.loadingToast(true, {title: "正在获取用户资料"});
+    // 此处需要用户授权,造成异步
+    wx.getUserInfo({
+      lang: "zh_CN",
+      encryptedData: true,
+      success: function(res) {
+        // wx.setStorageSync("userInfo",res.userInfo);
+        // eslint-disable-next-line
+        that.setData({
+          userInfo : res.userInfo,
+          isLoaded: true
+        });
+        console.log(that.data.userInfo);
+        util.loadingToast(false);
+      },
+      // 用户拒绝获取用户信息
+      fail: function() {
+        util.loadingToast(false);
+        util.dialog({
+          content: "您没有授权,无法获取信息",
+          showCancel: false
+        });
+        that.setData({
+          isLoaded: true
+        });
+        // 获取失败或者用户拒绝:后需要处理!
+        // that.userLogin(options);
+      },
+      complete: function() {
 
+      }
+    });
+  },
+  bindGetUserInfo: function() {
+    let that = this;
+    console.log("bindGetUserInfo");
+    wx.getUserInfo({
+      lang: "zh_CN",
+      encryptedData: true,
+      success: function(res) {
+        // wx.setStorageSync("userInfo",res.userInfo);
+        // eslint-disable-next-line
+        that.setData({
+          userInfo : res.userInfo,
+          isLoaded: true
+        });
+        // console.log(that.data.userInfo);
+        util.loadingToast(false);
+      }
+    });
+  },
   bindViewTap: function() {
 
+  },
+  exD: function() {
+    wx.navigateTo({
+      url: "../expressDelivery/expressDelivery"
+    })
   }
 });
